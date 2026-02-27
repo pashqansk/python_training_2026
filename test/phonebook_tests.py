d1809fe8cfaa4b9ebf3c15a9fa731d4b6dde1067
Тимошенко Павел <timoshenko@signatec.ru>
2026-02-27 13:27:00 +0700

    Код разделен на пакеты в соответствии с архитектурой проекта

--- test/phonebook_tests.py
+++ test/phonebook_tests.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
-from application import Application
-from group import Group
-from contact import Contact
+from fixture.application import Application
+from model.group import Group
+from model.contact import Contact
 
 
 @pytest.fixture
