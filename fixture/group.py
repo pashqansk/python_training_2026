fd10c450bc6c89d3f370df05b8068e5ae06d7454
Тимошенко Павел <timoshenko@signatec.ru>
2026-03-02 09:44:09 +0700

    добавил переход на нужную страницу в методах удаления и изменения. вернул набор параметров для тестов изменения контакта и группы

--- fixture/group.py
+++ fixture/group.py
@@ -3,11 +3,6 @@ class GroupHelper:
         self.app = app
 
 
-    def return_to_groups_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
-
-
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
@@ -26,7 +21,7 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
+        self.open_groups_page()
 
 
     def open_groups_page(self):
@@ -39,16 +34,23 @@ class GroupHelper:
         self.open_groups_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
+        self.open_groups_page()
 
 
-    def modify_first_group_name(self, group):
+    def modify_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
+        # init group edition
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
+        # edit group form
         wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group edition
         wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
+        self.open_groups_page()
