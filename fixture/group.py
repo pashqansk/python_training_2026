92b8845632061d96270f530b6a20f671d98eb792
Тимошенко Павел <timoshenko@signatec.ru>
2026-03-02 10:19:52 +0700

    Revert "выделена фикстура для всех тестов в файл conftest.py, файлы теста переименованы в test_*, создан файл для тестов удаления"
    This reverts commit 151412fe

--- fixture/group.py
+++ fixture/group.py
@@ -3,6 +3,11 @@ class GroupHelper:
         self.app = app
 
 
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
@@ -21,36 +26,9 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        self.open_groups_page()
+        self.return_to_groups_page()
 
 
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
-
-
-    def delete_first_group(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("delete").click()
-        self.open_groups_page()
-
-
-    def modify_first_group(self, group):
-        wd = self.app.wd
-        self.open_groups_page()
-        # init group edition
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("edit").click()
-        # edit group form
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group edition
-        wd.find_element_by_name("update").click()
-        self.open_groups_page()
