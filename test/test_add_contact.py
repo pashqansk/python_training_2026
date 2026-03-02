64d7abc887d6f4f0d28d4d68d818f8bf45139ceb
Тимошенко Павел <timoshenko@signatec.ru>
2026-03-02 11:37:58 +0700

    откат коммита для выделения теста контактов в отдельный файл.

---
+++ test/test_add_contact.py
@@ -0,0 +1,35 @@
+import pytest
+from fixture.application import Application
+from model.contact import Contact
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_contact(app):
+        app.session.login(username="admin", password="secret")
+        app.contact.create(Contact(
+            firstname="Vasya",
+            midname="Andreevich",
+            lastname="Pupkin",
+            nickname="Kosoi",
+            title="che tyt nado pisat",
+            workname="kgb",
+            homeaddr="Pushkina, 42",
+            cityphone="2174545",
+            mobilephone="79991112345",
+            workphone="88005553535",
+            email1="qwe@rty.io",
+            email2="asd@fgh.jk",
+            email3="zxc@vbn.mu",
+            url="https://ya.ru",
+            bday="1",
+            bmonth="January",
+            byear="1995",
+            aday="1",
+            amonth="January",
+            ayear="2025"))
+        app.session.logout()
