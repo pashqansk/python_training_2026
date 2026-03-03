24f2d43bfe5d5176ac176faf871af95498be3bfb
Тимошенко Павел <timoshenko@signatec.ru>
2026-03-04 00:07:07 +0700

    борьба с копипастой, выделил общие методы в для методов создания и изменения контактов и групп

--- fixture/contact.py
+++ fixture/contact.py
@@ -10,12 +10,7 @@ class ContactHelper:
         wd.find_element_by_link_text("home").click()
 
 
-    def create(self, contact):
-        wd = self.app.wd
-        # init contact creation
-        self.open_contacts_page()
-        wd.find_element_by_link_text("add new").click()
-        # fill contact form
+    def fill_contact_form(self, contact, wd):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
@@ -71,16 +66,17 @@ class ContactHelper:
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
         wd.find_element_by_name("ayear").send_keys(contact.ayear)
+
+
+    def create(self, contact):
+        wd = self.app.wd
+        # init contact creation
+        self.open_contacts_page()
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        self.fill_contact_form(contact, wd)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[19]").click()
-        self.open_contacts_page()
-
-
-    def delete_first_contact(self):
-        wd = self.app.wd
-        self.open_contacts_page()
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("delete").click()
         self.open_contacts_page()
 
 
@@ -91,61 +87,15 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         # edit contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.midname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.workname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.homeaddr)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.cityphone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.workphone)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.url)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
+        self.fill_contact_form(contact, wd)
         # submit contact edition
         wd.find_element_by_name("update").click()
+        self.open_contacts_page()
+
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        self.open_contacts_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
         self.open_contacts_page()
--- fixture/group.py
+++ fixture/group.py
@@ -3,18 +3,12 @@ class GroupHelper:
         self.app = app
 
 
-    def return_to_groups_page(self):
+    def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
 
-    def create(self, group):
-        wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
+    def fill_group_form(self, group, wd):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -24,11 +18,34 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group, wd)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
+        self.open_groups_page()
 
 
-    def open_groups_page(self):
+    def modify_first_group(self, group):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        self.open_groups_page()
+        # init group edition
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(group, wd)
+        # submit group edition
+        wd.find_element_by_name("update").click()
+        self.open_groups_page()
+
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
+        self.open_groups_page()
