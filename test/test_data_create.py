8c176e1b63151aded6e83573d40ca70913ee2f92
Тимошенко Павел <timoshenko@signatec.ru>
2026-03-04 00:51:11 +0700

    привёл всё в порядок после неудачного отката бранча до определенного коммита.

--- test/test_data_create.py
+++ test/test_data_create.py
@@ -1,13 +1,18 @@
-import pytest
-from fixture.application import Application
+from model.group import Group
 from model.contact import Contact
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+def test_add_group(app):
+        app.session.login(username="admin", password="secret")
+        app.group.create(Group(name="1345134534531t", header="qaegheryearhaerheasrseh", footer="asdhrehsdfhdhdrh"))
+        app.session.logout()
+
+
+def test_add_empty_group(app):
+        app.session.login(username="admin", password="secret")
+        app.group.create(Group())
+        app.session.logout()
+
 
 def test_add_contact(app):
         app.session.login(username="admin", password="secret")
