9a059590f2d36982decb4012deb44d1a6f83d73c
Тимошенко Павел <timoshenko@signatec.ru>
2026-03-06 18:38:28 +0700

    реализована умная проверка состояния сессии

--- fixture/session.py
+++ fixture/session.py
@@ -16,6 +16,32 @@ class SessionHelper:
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
 
+    def is_logged_in(self) -> bool:
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+
+    def is_logged_in_as(self, username) -> bool:
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "("+username+")"
+
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
