374ba0b7a925ef3734d6b9942c72e4d29994ebf2
Тимошенко Павел <timoshenko@signatec.ru>
2026-03-26 10:49:03 +0700

    Исправление. Убрал очистку символов в методе склейки Email в соответствии с поведением отображения в таблице на homepage

--- test/test_home_page_asserts.py
+++ test/test_home_page_asserts.py
@@ -42,6 +42,5 @@ def merge_phones_like_on_home_page(contact):
 def merge_emails_like_on_home_page(contact):
     return "\n".join(
         filter(lambda x: x != "",
-               map(lambda x: clean(x),
                      filter(lambda x: x is not None,
-                            [contact.email1, contact.email2, contact.email3]))))
+                            [contact.email1, contact.email2, contact.email3])))
