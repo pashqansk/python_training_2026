640ac3530e8d6d91173184f1f026a224ef5dff0a
Тимошенко Павел <timoshenko@signatec.ru>
2026-04-06 15:01:21 +0700

    Переделаны тесты изменения групп и контактов: проверка осуществляется на уровне БД

--- test/test_data_modify.py
+++ test/test_data_modify.py
@@ -1,51 +1,29 @@
-from random import randrange
+import random
 from model.group import Group
 from model.contact import Contact
 
 
-def test_modify_random_group_name(app):
-    if app.group.count() == 0:
+def test_modify_random_group_name(app, db):
+    if len(db.get_group_list()) == 0:
         app.group.create_group(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    # определяем индекс изменяемой группы случайным образом
-    index = randrange(len(old_groups))
-    group = Group(name="new_group_name")
-    group.id = old_groups[index].id
-    # передаем в качестве параметра порядковый номер изменяемой группы
-    app.group.modify_group_by_index(index, group)
-    new_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    app.group.modify_group_by_id(group.id, Group(name="new_group_name"))
+    new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[index] = group
+    modified_group = Group(id=group.id, name="new_group_name")
+    old_groups = [modified_group if g.id == group.id else g for g in old_groups]
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-# Этот тест будет удален после использования параметризованных тестов
-'''def test_modify_first_group_header(app):
-    if app.group.count() == 0:
-        app.group.create_group(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="new_group_header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)'''
-
-
-def test_modify_random_contact_name(app):
-    if app.contact.count() == 0:
+def test_modify_random_contact_name(app, db):
+    if len(db.get_contact_list()) == 0:
         app.contact.create_contact(Contact(firstname="test-Fname",lastname="test-Lname"))
-    # формируем список
-    old_contacts = app.contact.get_contact_list()
-    # рандомно определяем порядковый номер (id) изменяемого контакта
-    index = randrange(len(old_contacts))
-    contact = Contact(
-        firstname="new_firstname",
-        lastname="new_lastname")
-    contact.id = old_contacts[index].id
-    # в параметре передаем порядковый номер того контакта, который изменяем
-    app.contact.modify_contact_by_index(index, contact)
-    # формируем новый список с учётом измененного контакта
-    new_contacts = app.contact.get_contact_list()
-    # сверяем количество элементов в изначальном и новом списках
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    app.contact.modify_contact_by_id(contact.id, Contact(firstname="new_firstname", lastname="new_lastname"))
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    old_contacts[index] = contact
-    # сверяем отсортированные списки
+    modified_contact = Contact(id=contact.id, firstname="new_firstname", lastname="new_lastname")
+    old_contacts = [modified_contact if c.id == contact.id else c for c in old_contacts]
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
