4fae3252021c58a76deda9529de9e52575a4bad9
Тимошенко Павел <timoshenko@signatec.ru>
2026-04-06 19:15:23 +0700

    Отношения между объектами. Не работает получение списка контактов входящих в группы. Как и обратный метод.

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="localhost", name="addressbook", user="root", password="")
 
@@ -30,10 +31,28 @@ finally:
     pass #db.destroy()'''
 
 
-try:
+'''try:
     l = db.get_contact_list()
     for item in l:
         print(item)
     print(len(l))
 finally:
+    pass #db.destroy()'''
+
+
+try:
+    l = db.get_contacts_in_group(Group(id="11"))
+    for item in l:
+        print(item)
+    print(len(l))
+finally:
     pass #db.destroy()
+
+
+'''try:
+    l = db.get_contacts_not_in_group(Group(id="394"))
+    for item in l:
+        print(item)
+    print(len(l))
+finally:
+    pass #db.destroy()'''
