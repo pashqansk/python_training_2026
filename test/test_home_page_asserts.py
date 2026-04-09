55e556ce56bd07fcd7586dc375741722ffccf669
Тимошенко Павел <timoshenko@signatec.ru>
2026-04-09 15:44:30 +0700

    Переделал тест сравнения контактов на главной с использованием методов merge_phones_like_on_home_page и merge_emails_like_on_home_page. Задание 21

--- test/test_home_page_asserts.py
+++ test/test_home_page_asserts.py
@@ -1,36 +1,22 @@
 import re
-from random import randrange
 from model.contact import Contact
-
-def test_assert_random_credentials_on_home_page(app):
-    contacts = app.contact.get_contact_list()
-    index = randrange(len(contacts))
-    contact_from_home_page = contacts[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_edit_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_edit_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_edit_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
 
 def test_assert_all_credentials_on_home_page(app, db):
-    if len(db.get_contact_list()) == 0:
-        app.contact.create_contact(Contact(firstname="test"))
-    contacts_ui = app.contact.get_contact_list()
-    contacts_db = db.get_home_page_list()
-    assert len(contacts_ui) == len(contacts_db)
-    assert sorted(contacts_ui, key=Contact.id_or_max) == sorted(contacts_db, key=Contact.id_or_max)
-
-
-# TODO с ним надо что-то придумать, т.к. зависим от данных, которые могут обмануть паттерн в регулярке
-# TODO потом перенести в другой тестовый файл, работающий с view page
-def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.home == contact_from_edit_page.home
-    assert contact_from_view_page.work == contact_from_edit_page.work
-    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
+    contacts_from_home_page = app.contact.get_contact_list()
+    contacts_from_db = db.get_contact_list()
+    contacts_from_home_page_sorted = sorted(contacts_from_home_page, key=Contact.id_or_max)
+    contacts_from_db_sorted = sorted(contacts_from_db, key=Contact.id_or_max)
+    for i in range(len(contacts_from_home_page_sorted)):
+        contact_from_home_page = contacts_from_home_page_sorted[i]
+        contact_from_db = contacts_from_db_sorted[i]
+        assert contact_from_home_page.firstname == contact_from_db.firstname
+        assert contact_from_home_page.lastname == contact_from_db.lastname
+        assert contact_from_home_page.address == contact_from_db.address
+        merged_phones = merge_phones_like_on_home_page(contact_from_db)
+        merged_emails = merge_emails_like_on_home_page(contact_from_db)
+        assert contact_from_home_page.all_phones_from_home_page == merged_phones
+        assert contact_from_home_page.all_emails_from_home_page == merged_emails
 
 
 def clean(s):
